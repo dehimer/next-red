ee99ee3b7c25dc50a877addec01ca08a0b1e525a
Denis Novikov <den-vse@yandex.ru>
2019-11-05 02:37:05 +0300

    feature: prepare layout areas

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -3,42 +3,60 @@ import Bottom from '../Bottom';
 import BGImage from './bg.png';
 
 const Layout = props => (
-  <div className="layout">
-    <Header />
-    {props.children}
-    <Bottom />
-    <img src={BGImage} alt=""/>
+    <div className="layout">
+        <div className="header" >
+            <Header/>
+        </div>
+        <div className="content">
+            {props.children}
+        </div>
+        <div className="bottom">
+            <Bottom />
+        </div>
+        <img src={BGImage} alt="" />
 
-    { /*language=CSS*/ }
-    <style jsx>{`
-        .layout {
-          background: linear-gradient(0.4turn, #a81e23, #781214 30%, #781214);
-          width: 100vw;
-          max-width: 500px;
-          height: 100vh;
-          margin: 0 auto;
-        }
-        
-        img {
-            width: 500px;
-            bottom: 0;
-            position: absolute;
-            opacity: 0.07;
-        }
-    `}
-    </style>
-    { /*language=CSS*/ }
-    <style jsx global>{`
-      body {
-        margin: 0;
-        padding: 0;
-        background-color: #781214;
-        font-family: "Roboto Mono Medium for Powerline", sans-serif;
-      }
-    `}
-    </style>
+        { /*language=CSS*/ }
+        <style jsx>{`
+            .layout {
+              background: linear-gradient(0.4turn, #a81e23, #781214 30%, #781214);
+              width: 100vw;
+              max-width: 500px;
+              height: 100vh;
+              margin: 0 auto;
+            }
+            
+            img {
+                width: 500px;
+                bottom: 0;
+                position: absolute;
+                opacity: 0.07;
+            }
+            
+            .header {
+                height: 15%;
+            }
+            
+            .content {
+                height: 65%;
+            }
+            
+            .bottom {
+                height: 20%;
+            }
+        `}
+        </style>
+        { /*language=CSS*/ }
+        <style jsx global>{`
+          body {
+            margin: 0;
+            padding: 0;
+            background-color: #781214;
+            font-family: "Roboto Mono Medium for Powerline", sans-serif;
+          }
+        `}
+        </style>
 
-  </div>
+    </div>
 );
 
 export default Layout;
